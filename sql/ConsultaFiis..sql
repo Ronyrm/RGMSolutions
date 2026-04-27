cb26a239509efa8d076b2fd77400634ac4439243
Ronyrm <ronygustavo_rm@yahoo.com.br>
2026-04-27 09:26:04 -0300

    atualização sql fiis e inserts geral

--- sql/ConsultaFiis..sql
+++ sql/ConsultaFiis..sql
@@ -1,7 +1,8 @@
 SELECT 
-f.*
+GROUP_CONCAT(concat(F.ticker,'') ORDER BY F.ticker)
 from fiis f 
-WHERE f.dy > 8 AND f.p_vp BETWEEN 0.8 AND 0.99
-AND f.liquidez_media_diaria >= 5000000
+WHERE 
+(f.dy > 8 AND f.p_vp BETWEEN 0.8 AND 0.99
+AND f.liquidez_media_diaria >= 5000000)  
+/*OR f.ticker IN ('ALZR11','CPTS11','HGLG11','KNCR11','KNRI11','RECR11','XPML11')*/
 ORDER BY f.dy DESC, f.liquidez_media_diaria
-/* http://127.0.0.1:5000/get/fiis */; 
